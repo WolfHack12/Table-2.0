325203ae2257849640c5850e2fb3448f858e775c
Timofey Datskevich <tim.datskevich@mail.ru>
2023-09-17 17:24:36 +0300

    feat: add column

--- Buttons/ButtonsFunctor.js
+++ Buttons/ButtonsFunctor.js
@@ -1,8 +1,13 @@
-function AddRows(){
+function addRows(){
     let str = document.createElement('tr');
     for(let i = 1; i<=table.rows[0].cells.length;i++){
         let td = document.createElement('td');
         str.appendChild(td);
     };
    table.firstChild.appendChild(str);
+}
+function addColumn(){
+    for(let i = 0; i < table.rows.length; i++){
+        table.rows[i].insertCell(-1);
+    }
 }
