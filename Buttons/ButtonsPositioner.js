4c4520c8714f51810d24fbb6aa7d556600679f71
Timofey Datskevich <tim.datskevich@mail.ru>
2023-09-17 19:34:41 +0300

    Add and first pos minuses

--- Buttons/ButtonsPositioner.js
+++ Buttons/ButtonsPositioner.js
@@ -1,16 +1,29 @@
 var buttonPlus1 = document.getElementById("plus1");
 var buttonPlus2 = document.getElementById("plus2");
+var buttonMinus1 = document.getElementById("minus1");
+var buttonMinus2 = document.getElementById("minus2");
 var table = document.querySelector("table");
 function defaultPosPluses(){
-    let firstCell = table.rows[table.rows.length - 1].cells[0];
+    let firstCellLastedRow= table.rows[table.rows.length - 1].cells[0];
     let lastCell = table.rows[0].cells[table.rows[0].cells.length - 1];
-    let coords1 = firstCell.getBoundingClientRect();
+    let coords1 = firstCellLastedRow.getBoundingClientRect();
     let coords2 = lastCell.getBoundingClientRect();
-    buttonPlus1.style.top = (coords1.top + firstCell.clientWidth + 5) + "px";
+    buttonPlus1.style.top = (coords1.top + firstCellLastedRow.clientHeight + 5) + "px";
     buttonPlus1.style.left = coords1.left + "px";
     buttonPlus2.style.top = coords2.top + "px";
     buttonPlus2.style.left = (coords2.left + lastCell.clientWidth + 5) + "px";
 }
-window.onload = defaultPosPluses;
 buttonPlus1.addEventListener('click', defaultPosPluses);
 buttonPlus2.addEventListener('click', defaultPosPluses);
+
+function defaultPosMinuses(){
+    let firstCell = table.rows[0].cells[0];
+    let coords = firstCell.getBoundingClientRect();
+    buttonMinus1.style.top = (coords.top - firstCell.clientHeight - 5) + "px";
+    buttonMinus1.style.left = coords.left + "px";
+    buttonMinus2.style.top = coords.top + "px";
+    buttonMinus2.style.left = (coords.left - firstCell.clientHeight) - 5 + "px";
+    
+}
+defaultPosPluses();
+defaultPosMinuses();
